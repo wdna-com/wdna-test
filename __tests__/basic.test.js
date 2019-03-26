de28db799c22fd1db0614ab098a09dd117a9d9b6
Reinier César Mujica Hernández <reinier.mujica@gmail.com>
2019-03-26 10:51:08 +0100

    added test

---
+++ __tests__/basic.test.js
@@ -0,0 +1,15 @@
+const { run } = require("../src/runner");
+
+const { expect } = require("chai");
+
+describe("Runner Test", () => {
+  it("Should return the double", done => {
+    const steps = 10;
+
+    const output = run(steps);
+    const expected = steps * 2;
+
+    expect(output).to.equal(expected);
+    done();
+  });
+});
---
+++ src/runner.js
@@ -0,0 +1,5 @@
+function run(steps) {
+  return steps * 2;
+}
+
+module.exports = { run: run };
